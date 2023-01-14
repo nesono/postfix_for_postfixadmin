ba563e639178478a6bfd3ee98c8f2a6bc519b20a
Jochen Issing <c.333+git@nesono.com>
2023-01-14 09:15:23 +0100

    Remove invalid user spec

--- scripts/run.sh
+++ scripts/run.sh
@@ -57,7 +57,7 @@ if [[ -n "${SPF_ENABLE:-}" ]]; then
   do_postconf -e 'policyd-spf_time_limit=3600'
   cat <<EOF >> /etc/postfix/master.cf
 policyd-spf  unix  -       n       n       -       0       spawn
-    user=policyd-spf argv=/usr/bin/policyd-spf
+    argv=/usr/bin/policyd-spf
 EOF
 fi
 
